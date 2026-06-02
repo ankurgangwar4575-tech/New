9dc9e094adc55bba34fd0280fa10088af34844f6
Ankur Gangwar <ankurgangwar4575@gmail.com>
2026-06-02 19:51:14 +0530

    api

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 import dotenv from "dotenv";
 import connectDB from "./src/db/db.js";
-
+import app from "./src/app.js";
 dotenv.config({
-    path:"./env"
+  path: "./env",
 });
-
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log("Server is running on port ", process.env.PORT);
+    });
+  })
+  .catch((err) => {
+    console.log(`MongoDB connection error : ${err}`);
+  });
