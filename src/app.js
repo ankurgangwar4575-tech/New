9d00d437ee3401dcc0255c8c84dcd7fe496e480e
Ankur Gangwar <ankurgangwar4575@gmail.com>
2026-06-04 11:09:45 +0530

    postman

--- src/app.js
+++ src/app.js
@@ -2,10 +2,10 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 const app=express();
-app.use({
+app.use(cors({
     origin:process.env.CORS_ORIGIN,
     credentials:true
-});
+}));
 app.use(express.json({
     limit:"16kb"
 }));
@@ -15,4 +15,12 @@ app.use(express.urlencoded({
 }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+
+// routes import
+import userRouter from './routes/user.routes.js'
+
+// routes declaration
+app.use("/api/v1/users",userRouter);
+
 export default app;
