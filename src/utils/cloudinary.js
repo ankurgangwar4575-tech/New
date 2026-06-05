6f2fd39eb0b2f7aefc15daf35f836d119e702c7c
Ankur Gangwar <ankurgangwar4575@gmail.com>
2026-06-05 15:00:20 +0530

    c

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -15,6 +15,7 @@ const uploadOnCloudinary = async (localFilePath) => {
     });
     // file has been uploaded successfully
     console.log("File has been uploaded on cloudinary", response.url);
+    fs.unlinkSync(localFilePath);
     return response;
   } catch (err) {
     fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload operation got failed
